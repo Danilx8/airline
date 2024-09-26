a355e0431e7647d8386e65de90e5f3454a2d94ad
Danilx8 <lugovskihdanil@yandex.ru>
2024-09-26 21:11:04 +0300

    Добавил логгирование ошибок

--- app/api/controller/user_controller.go
+++ app/api/controller/user_controller.go
@@ -4,6 +4,7 @@ import (
 	"app/app/domain"
 	"app/app/usecase"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 )
 
@@ -16,9 +17,20 @@ const (
 	EMPLOYEES = false
 )
 
+// GetEmployeeUsers airline
+//
+//	@Summary		Get all users in system that are not administrators
+//	@Description	get non-administrator users entities
+//	@ID				get-non-administrator-users
+//	@Accept			json
+//	@Produce		json
+//	@Success		200	{object}	domain.User		"ok"
+//	@Failure		500	{object}	web.APIError	"error"
+//	@Router			/users [get]
 func (userController *UserController) GetEmployeeUsers(c *gin.Context) {
 	users, err := userController.UserUsecase.FetchAll(c, EMPLOYEES)
 	if err != nil {
+		log.Print(err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
@@ -31,6 +43,7 @@ func (userController *UserController) CreateUser(c *gin.Context) {
 
 	err := c.BindJSON(&user)
 	if err != nil {
+		log.Print(err.Error())
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -38,6 +51,7 @@ func (userController *UserController) CreateUser(c *gin.Context) {
 	var userId int64
 	userId, err = userController.UserUsecase.CreateUser(c, user)
 	if err != nil {
+		log.Print(err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
 	c.JSON(http.StatusOK, gin.H{"userId": userId})
@@ -48,12 +62,14 @@ func (userController *UserController) UpdateUser(c *gin.Context) {
 
 	err := c.BindJSON(&user)
 	if err != nil {
+		log.Print(err.Error())
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	err = userController.UserUsecase.UpdateUser(c, user)
 	if err != nil {
+		log.Print(err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
@@ -65,11 +81,13 @@ func (userController *UserController) DeleteUser(c *gin.Context) {
 	var id int64
 	err := c.BindJSON(&id)
 	if err != nil {
+		log.Print(err.Error())
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 	}
 
 	err = userController.UserUsecase.DeleteUser(c, id)
 	if err != nil {
+		log.Print(err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
