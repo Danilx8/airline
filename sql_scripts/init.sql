cabf7597ec57f1de8b3f1b1eeafe6eb197452262
mptantaa <129151341+mptantaa@users.noreply.github.com>
2024-10-08 21:49:56 +0300

    Add in init.sql status ban, reason ban

--- sql_scripts/init.sql
+++ sql_scripts/init.sql
@@ -106,6 +106,8 @@ CREATE TABLE `users` (
   `OfficeID` int(11) DEFAULT NULL,
   `Birthdate` date DEFAULT NULL,
   `Active` int(2) DEFAULT NULL,
+  `Banned` BOOLEAN DEFAULT 0,
+  `BanReason` TEXT DEFAULT NULL,
   PRIMARY KEY (`ID`),
   FOREIGN KEY (`Active`) REFERENCES `user_status` (`ID`),
   KEY `FK_Users_Offices` (`OfficeID`),
@@ -177,14 +179,14 @@ LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 -- SET GLOBAL local_infile = 1;
 INSERT INTO users (ID, RoleID, Email, Password, FirstName, LastName, OfficeID, Birthdate, Active) VALUES
-	('1', '1', 'j.doe@amonic.com', '123', 'John', 'Doe', '1', '1983-01-13', '1'),
-	('2', '2', 'k.omar@amonic.com', '4258', 'Karim', 'Omar', '1', '1980-03-19', '1'),
-	('3', '2', 'h.saeed@amonic.com', '2020', 'Hannan', 'Saeed', '3', '1989-12-20', '1'),
-	('4', '2', 'a.hobart@amonic.com', '6996', 'Andrew', 'Hobart', '6', '1990-01-30', '1'),
-	('5', '2', 'k.anderson@amonic.com', '4570', 'Katrin', 'Anderson', '5', '1992-11-10', '1'),
-	('6', '2', 'h.wyrick@amonic.com', '1199', 'Hava', 'Wyrick', '1', '1988-08-08', '1'),
-	('7', '2', 'marie.horn@amonic.com', '55555', 'Marie', 'Horn', '4', '1981-04-06', '1'),
-	('8', '2', 'm.osteen@amonic.com', '9800', 'Milagros', 'Osteen', '1', '1991-02-03', '0');
+    (1, 1, 'j.doe@amonic.com', '123', 'John', 'Doe', 1, '1983-01-13', 1, 0, NULL),
+    (2, 2, 'k.omar@amonic.com', '4258', 'Karim', 'Omar', 1, '1980-03-19', 1, 0, NULL),
+    (3, 2, 'h.saeed@amonic.com', '2020', 'Hannan', 'Saeed', 3, '1989-12-20', 1, 0, NULL),
+    (4, 2, 'a.hobart@amonic.com', '6996', 'Andrew', 'Hobart', 6, '1990-01-30', 1, 0, NULL),
+    (5, 2, 'k.anderson@amonic.com', '4570', 'Katrin', 'Anderson', 5, '1992-11-10', 1, 0, NULL),
+    (6, 2, 'h.wyrick@amonic.com', '1199', 'Hava', 'Wyrick', 1, '1988-08-08', 1, 0, NULL),
+    (7, 2, 'marie.horn@amonic.com', '55555', 'Marie', 'Horn', 4, '1981-04-06', 1, 0, NULL),
+    (8, 2, 'm.osteen@amonic.com', '9800', 'Milagros', 'Osteen', 1, '1991-02-03', 0, NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
